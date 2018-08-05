74c6f2e206f0063adcb5eae66c0b68220a9577a6
Ina Carine <inakara89@gmail.com>
2018-08-05 13:07:28 +0100

    Add comments

--- src/utils/GoogleAPI.js
+++ src/utils/GoogleAPI.js
@@ -1,7 +1,13 @@
 const api = 'https://maps.googleapis.com/maps/api/js';
 const key = 'AIzaSyDp6EaOjIVW_TlxSgwbqkuMTdS3RzADPe8';
 
+/*
+* @description: Loads the Google Map API script and
+* adds it to the page
+*/
 export const load = () => {
+  // Creates a promise to make sure the script gets loaded
+  // before the api gets used
   return new Promise((resolve, reject) => {
     const root = document.getElementById('root');
     const googleScriptTag = document.createElement('script');
